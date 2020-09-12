a64454d998f747df607e8c3d9c5c140f7c40943e
dzmitrydabrahost <dzmitrydabrahost@gmail.com>
2020-09-12 17:38:12 +0300

    feat: add final result

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+
+    function checkToBracket(expr) {
+        let openBracket = 0;
+        let closeBracket = 0;
+        for(let i = 0; i < expr.length; i++) {
+            if(expr[i] === '(') openBracket += 1;
+            else if(expr[i] === ')') closeBracket += 1;
+        }
+        if(openBracket === closeBracket) return true;
+        else return false;
+    }
+
+    if (!checkToBracket(expr)) throw new Error ("ExpressionError: Brackets must be paired"); 
+    if (expr.indexOf('/0') != -1 || expr.indexOf('/ 0') != -1) throw new Error ('TypeError: Division by zero.');
+    expr = expr.replace(/ /g, '') && new Function(`return ${expr}`);
+    return expr();
 }
 
 module.exports = {
